d47ea17834012b55b3501b734abfcb7e77642f9f
liulongchao <liulongchao@gomeholdings.com>
2017-06-22 10:29:34 +0800

    Rx系列--辅助操作

--- app/src/main/java/com/example/llcgs/android_rx/rxjava/EightActivity.java
+++ app/src/main/java/com/example/llcgs/android_rx/rxjava/EightActivity.java
@@ -5,20 +5,26 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.example.llcgs.android_rx.R;
+import com.example.llcgs.android_rx.rxbinding.MyObserver;
 import com.example.llcgs.android_rx.rxlifecycle.ActivityLifeCycleEvent;
 import com.example.llcgs.android_rx.rxlifecycle.BaseActivity;
 
+import java.util.Arrays;
+import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Notification;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.ObservableSource;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.schedulers.Timed;
 
 /**
  * com.example.llcgs.android_rx.rxjava.EightActivity
@@ -141,9 +147,116 @@ public class EightActivity extends BaseActivity {
 
         /**
          * Serialize
-         *   强制一个Observable连续调用并保证行为正确
+         *   强制一个Observable连续调用并保证行为正确且是同步的
+         *   ""
+         * */
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                e.onNext("Where");
+                e.onNext("amazing");
+                e.onComplete();
+                e.onNext("happens");
+                e.onComplete();
+            }
+            // 同步执行？？？？
+        }).serialize().subscribe(new Consumer<String>() {
+            @Override
+            public void accept(@NonNull String s) throws Exception {
+                Log.d(TAG, "s:" + s);
+            }
+        });
+
+        /**
+         * TimeInterval
+         *  将一个发射数据的Observable转换为发射那些数据发射时间间隔的Observable
+         * */
+        Observable.fromArray(nbaArray).timeInterval().subscribe(new Consumer<Timed<String>>() {
+            @Override
+            public void accept(@NonNull Timed<String> stringTimed) throws Exception {
+                Log.d(TAG, "value: " + stringTimed.value()+ ", time: "+stringTimed.time() + ", unit: "+stringTimed.unit());
+            }
+        });
+
+        /**
+         * Timeout
+         *    对原始Observable的一个镜像，如果过了一个指定的时长仍没有发射数据，它会发一个错误通知
+         * */
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                e.onNext("play football");
+                //Thread.sleep(4000);
+                e.onNext("paly baseketball");
+                e.onComplete();
+            }
+            // 休眠了四秒 再发射paly baseketball timeout限制的是3秒 所以 onError发射通知结束
+        }).timeout(3, TimeUnit.SECONDS).subscribe(new MyObserver<String>() {
+            @Override
+            public void onNext(@NonNull String s) {
+                Log.d(TAG, "s: " +s);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                e.printStackTrace();
+            }
+        });
+
+        /**
+         * Timestamp
+         *  给Observable发射的数据项附加一个时间戳
+         * */
+        Observable.fromIterable(Arrays.asList(nbaArray))
+                .timestamp()
+                .subscribe(new Consumer<Timed<String>>() {
+                    @Override
+                    public void accept(@NonNull Timed<String> stringTimed) throws Exception {
+                        Log.d(TAG, "timestamp():"+"value: " + stringTimed.value()+ ", time: "+stringTimed.time() + ", unit: "+stringTimed.unit());
+                    }
+                });
+
+        /**
+         * using
+         *  创建一个只在Observable生命周期内存在的一次性资源
+         *
+         *   before using被 use using冲掉了
          *
          * */
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(@NonNull ObservableEmitter<String> e) throws Exception {
+                e.onNext("before using");
+            }
+        }).using(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "use using";
+            }
+        }, new Function<String, ObservableSource<String>>() {
+            @Override
+            public ObservableSource<String> apply(@NonNull String s) throws Exception {
+                return Observable.just("s: " + s);
+            }
+            // 订阅到的是new Callable<String>() 发射的结果
+        }, new Consumer<String>() {
+            @Override
+            public void accept(@NonNull String s) throws Exception {
+                Log.d(TAG, "using accept:" + s);
+            }
+            // 订阅到的是 Function<String, ObservableSource<String>>() 这个Observable发射的结果
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(@NonNull String s) throws Exception {
+                Log.d(TAG, "subscribe accept:" + s);
+            }
+        });
+
+        /**
+         * to
+         *   将Observable转换为另一个对象或数据结构
+         * */
+
 
     }
 }
